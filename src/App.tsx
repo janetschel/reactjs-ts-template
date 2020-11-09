a6a219dfe1bfdf545cdaf0a166f58cdb768d3170
Jan Etschel <46886724+janetschel@users.noreply.github.com>
2020-11-09 21:31:26 +0100

    Initialized blueprint repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Typography} from "@material-ui/core";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Typography>Just the blueprint of a project...</Typography>
     </div>
   );
 }
